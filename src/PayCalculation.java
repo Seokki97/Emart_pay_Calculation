8294b09bf93aa6ba136909d54181d063c0c31490
programmers1997 <skaksdl1238@gmail.com>
2022-01-11 16:26:00 +0900

    commit

--- src/PayCalculation.java
+++ src/PayCalculation.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 class PayCalculation { //실제 급여계산을 이루어주는 클래스
     Calendar C = new Calendar();
-    StartDayFunction C3 = new StartDayFunction();
+    StartDayFunction C1 = new StartDayFunction();
     static int pay = 9160;
     int mon=0, tue=0, wed=0,thur=0,fri=0,sat=0,sun=0;
     int mon_result=0, tue_result=0, wed_result=0,
@@ -13,7 +13,7 @@ class PayCalculation { //실제 급여계산을 이루어주는 클래스
     int monStart=0,tueStart=0,wedStart=0,
             thurStart=0,friStart=0,satStart=0,sunStart=0;
     int[] SEVEN2 = C.SEVEN;
-    int[] dayCount2 = C3.dayCount;
+    int[] dayCount2 = C1.dayCount;
     public void startDayConcept() { // 1일이 아닌 중간에 들어와서 근무를 시작한 경우의 메소드
         monStart = SEVEN2[0] - dayCount2[0];
         tueStart = SEVEN2[1] - dayCount2[1];
