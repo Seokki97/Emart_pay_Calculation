d745307fe400fc81bad756120adedbc172c5a99e
programmers1997 <skaksdl1238@gmail.com>
2022-01-20 20:16:22 +0900

    commit

--- src/Execute.java
+++ src/Execute.java
@@ -6,12 +6,12 @@ public class Execute {
         c2.leapYear();
         c2.startFind();
         c2.Hyun_song();
-       /* c2.Hee_jun();
+        c2.Hee_jun();
         c2.Chae_young();
         c2.Jae_lim();
         c2.Yu_won();
         c2.Min_ji();
         c2.Se_bin();
-        c2.Chae_eun();*/
+        c2.Chae_eun();
     }
 }
