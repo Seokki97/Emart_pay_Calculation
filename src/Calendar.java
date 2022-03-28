6b3b452b0f89139d9532fc842b4caaca63ddaa04
programmers1997 <skaksdl1238@gmail.com>
2022-03-28 17:17:33 +0900

    변수이름 변경

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있는지를 나타내는 클래스
-    Scanner sc = new Scanner(System.in);
     int[] days = new int[12];
     int year, month, monthDay;
-    int[] SEVEN = new int[7];
+    int[] dayWeek = new int[7];
     String firstDay ;
     public void input(int year, int month, String firstDay) { //년 월, 시작요일을 입력받는 메소드
         this.year = year;
@@ -11,25 +10,25 @@ public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있
         this.firstDay= firstDay;
     }
 
-    public void leapYear() { //윤년인지 아닌지 판별해주는 메소드
+    public void setLeapYear() { //윤년인지 아닌지 판별해주는 메소드
         if( year % 4==0) {
             days = new int[] {31,29,31,30,31,30,31,31,30,31,30,31};
         }else {
             days = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
         }
-        for(int i=0; i<SEVEN.length; i++) {
-            SEVEN[i] = 4;
+        for(int i=0; i<dayWeek.length; i++) {
+            dayWeek[i] = 4;
         }
     }
 
     public void startMon() { //input값에 따라 해당 월의 월요일의 개수를 counting
         if(monthDay == 31) {
             for(int i=0; i<3; i++) {
-                SEVEN[i] +=1;
+                dayWeek[i] +=1;
             }
             if(monthDay == 30) {
                 for(int j =0; j<2 ; j++) {
-                    SEVEN[j] +=1;
+                    dayWeek[j] +=1;
                 }
             }
         }
@@ -38,11 +37,11 @@ public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있
     public void startTue() {//input값에 따라 해당 월의 화요일의 개수를 counting
         if(monthDay == 31) {
             for(int i = 1; i < 4; i++) {
-                SEVEN[i] +=1;
+                dayWeek[i] +=1;
             }
             if(monthDay == 30) {
                 for(int j =1; j<3; j++) {
-                    SEVEN[j] +=1;
+                    dayWeek[j] +=1;
                 }
             }
         }
@@ -51,11 +50,11 @@ public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있
     public void startWed() {//input값에 따라 해당 월의 수요일의 개수를 counting
         if(monthDay == 31) {
             for(int i = 2; i<5; i++) {
-                SEVEN[i] +=1;
+                dayWeek[i] +=1;
             }
             if(monthDay == 30) {
                 for(int j =2; j<4; j++) {
-                    SEVEN[j] +=1;
+                    dayWeek[j] +=1;
                 }
             }
         }
@@ -64,11 +63,11 @@ public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있
     public void startThur() {//input값에 따라 해당 월의 목요일의 개수를 counting
         if(monthDay==31) {
             for(int i=3; i<6; i++) {
-                SEVEN[i] +=1;
+                dayWeek[i] +=1;
             }
             if(monthDay == 30) {
                 for(int j=3; j<5; j++) {
-                    SEVEN[j] +=1;
+                    dayWeek[j] +=1;
                 }
             }
         }
@@ -77,11 +76,11 @@ public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있
     public void startFri() {//input값에 따라 해당 월의 금요일의 개수를 counting
         if(monthDay==31) {
             for(int i=4; i<7; i++) {
-                SEVEN[i] +=1;
+                dayWeek[i] +=1;
             }
             if(monthDay==30) {
                 for(int j=4; j<6; j++) {
-                    SEVEN[j] +=1;
+                    dayWeek[j] +=1;
                 }
             }
         }
@@ -90,25 +89,25 @@ public class Calendar { //입력한 달에 월요일~일요일이 각 몇번 있
     public void startSat() {//input값에 따라 해당 월의 토요일의 개수를 counting
         if(monthDay==31) {
             for(int i=5; i<7; i++) {
-                SEVEN[i]+=1;
+                dayWeek[i]+=1;
             }
-            SEVEN[0] += 1 ;
+            dayWeek[0] += 1 ;
         }
         if(monthDay==30) {
             for(int j =5; j<7; j++) {
-                SEVEN[j]+=1;
+                dayWeek[j]+=1;
             }
         }
     }
 
     public void startSun() {//input값에 따라 해당 월의 일요일의 개수를 counting
         if(monthDay==31) {
-            SEVEN[6] +=1;
-            SEVEN[0] +=1;
-            SEVEN[1] +=1;
+            dayWeek[6] +=1;
+            dayWeek[0] +=1;
+            dayWeek[1] +=1;
             if(monthDay==30) {
-                SEVEN[6] +=1;
-                SEVEN[0] +=1;
+                dayWeek[6] +=1;
+                dayWeek[0] +=1;
             }
         }
     }
--- src/PayCalculation.java
+++ src/PayCalculation.java
@@ -1,7 +1,7 @@
 class PayCalculation extends StartDayFunction{ //실제 급여계산을 이루어주는 클래스
     static int pay = 9160;
-    int mon_result=0, tue_result=0, wed_result=0,
-            thur_result=0, fri_result=0, sat_result=0, sun_result=0 ;
+    int monResult=0, tueResult=0, wedResult=0,
+            thurResult=0, friResult=0, satResult=0, sunResult=0 ;
     int monStart,tueStart,wedStart,
             thurStart,friStart,satStart,sunStart;
     int mon,tue,wed,thur,fri,sat,sun;
@@ -13,24 +13,24 @@ class PayCalculation extends StartDayFunction{ //실제 급여계산을 이루�
         share = startDayInput / 7;
         remainder = (startDayInput % 7);
         input(2022, 1, "토");
-        leapYear();
+        setLeapYear();
         startFind();
         endFind();
     }
 
     public void startDayConcept() {
       inputTotal();
-        monStart = SEVEN[0] - dayCount[0];
-        tueStart = SEVEN[1] - dayCount[1];
-        wedStart = SEVEN[2] - dayCount[2];
-        thurStart = SEVEN[3] - dayCount[3];
-        friStart = SEVEN[4] - dayCount[4];
-        satStart = SEVEN[5] - dayCount[5];
-        sunStart = SEVEN[6] - dayCount[6];
+        monStart = dayWeek[0] - dayCount[0];
+        tueStart = dayWeek[1] - dayCount[1];
+        wedStart = dayWeek[2] - dayCount[2];
+        thurStart = dayWeek[3] - dayCount[3];
+        friStart = dayWeek[4] - dayCount[4];
+        satStart = dayWeek[5] - dayCount[5];
+        sunStart = dayWeek[6] - dayCount[6];
 
     }
 
-    public void totalPay(){
+    public void showTotalPay(){
         System.out.println(this.name+"의 총 급여는" +result+"입니다.");
         for(int i = 0 ; i < dayCount.length; i++){
             System.out.println(dayCount[i]);
@@ -46,14 +46,14 @@ class PayCalculation extends StartDayFunction{ //실제 급여계산을 이루�
     public void result(){
         startDayConcept();
 
-        mon_result = this.mon*monStart*pay; tue_result = this.tue*tueStart*pay;
-        wed_result = this.wed*wedStart*pay; thur_result = this.thur*thurStart*pay;
-        fri_result = this.fri*friStart*pay; sat_result = this.sat*satStart*pay;
-        sun_result = this.sun*sunStart*pay;
+        monResult = this.mon*monStart*pay; tueResult = this.tue*tueStart*pay;
+        wedResult = this.wed*wedStart*pay; thurResult = this.thur*thurStart*pay;
+        friResult = this.fri*friStart*pay; satResult = this.sat*satStart*pay;
+        sunResult = this.sun*sunStart*pay;
 
-        result = mon_result + tue_result + wed_result + thur_result
-                +fri_result + sat_result + sun_result;
+        result = monResult + tueResult + wedResult + thurResult
+                +friResult + satResult + sunResult;
 
-        totalPay();
+        showTotalPay();
     }
     }
