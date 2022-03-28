c3b4de04e941aeea0043115af9696675bde39cb0
programmers1997 <skaksdl1238@gmail.com>
2022-03-28 16:12:03 +0900

    워커 추가

--- src/Worker.java
+++ src/Worker.java
@@ -4,6 +4,9 @@ class Worker { //근무자들의 정보를 담아낸 클래스
     PayCalculation heeJun =  new PayCalculation("희준", 5, 5, 0 ,0,0,9,8,1);
     PayCalculation jaeLim = new PayCalculation("재림",0,0,0,0,10,4,0,1);
     PayCalculation minJi = new PayCalculation("민지",7,7,0,0,0,0,0,1);
+    PayCalculation siYoung = new PayCalculation("시영",3,3,4,4,0,0,0,1);
+    PayCalculation jiMin = new PayCalculation("지민",0,0,8,4,0,0,0,1);
+    PayCalculation jungWon = new PayCalculation("정원", 0,0,0,4,10,0,0,1);
 
 }
 
