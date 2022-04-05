0b29e0262a733d7de30b298626e46cf1217d2a83
programmers1997 <skaksdl1238@gmail.com>
2022-04-05 17:21:15 +0900

    변수이름 변경

--- src/Worker.java
+++ src/Worker.java
@@ -1,5 +1,5 @@
 class Worker { //근무자들의 정보를 담아낸 클래스
-    PayCalculation hyunSong = new PayCalculation("현송",4,4,3,3,0,0,0,4,6,0);
+    PayCalculation hyunSong = new PayCalculation("현송",4,4,3,3,0,0,0,7,6,0);
     PayCalculation heeJun =  new PayCalculation("희준",5, 5, 0 ,0,0,5,8,1,6,0);
     PayCalculation jaeLim = new PayCalculation("재림", 0,0,0,0,9,4,0,1,11,6);
     PayCalculation minJi = new PayCalculation("민지",7,7,0,0,0,0,0,1,3,0);
